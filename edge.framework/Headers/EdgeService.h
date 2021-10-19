fc3a2a99bd98fbdbbbde92404375ae7e2aba5c7b
Radúz Benický <raduz.benicky@mimik.com>
2021-10-19 13:46:49 -0700

    13.0 initial commit of Xcode 13 compatible MIMIKEdgeClient library with vendored edgeEngine framework

---
+++ edge.framework/Headers/EdgeService.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <map>
+
+class EdgeService {
+public:
+    static int start(int port, const std::string& nodeId, const std::string& nodeName,
+        const std::string& license,
+        const std::string& workingPath,
+        const std::map< std::string, std::string>& edgeEnv);
+    static void stop();
+
+    static std::string getEdgeIdToken(int port, const std::string& nodeId,
+                       const std::string& nodeName, const std::string& license,
+                       const std::string& workingPath,
+                       const std::map<std::string, std::string>& optionMap);
+};
